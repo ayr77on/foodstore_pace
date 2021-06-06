a6b57a993bd58e921e6b46ac68245e054fdbddd3
Ayr77on <ayrtongp77@gmail.com>
2021-06-06 00:54:30 -0300

    Contador de items con stock validado

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './ItemCount.css';
+
+const ItemCount = ({initial,stock,onAdd}) => {
+    const [items, setContador] = useState(initial);
+    const addItem = () => {
+        if(items < stock){
+            setContador(items+1)
+        }else{
+            alert("No podes agregar mas productos");
+        }
+    };    
+    return (
+        <div className="justify-content-between align-items-center">
+            <div className="flex-container">
+                <input type="button" onClick={() => {setContador(items-1)}} value="-" className="minus btn-floating btn-large waves-effect waves-light red"/>
+                <input type="number" name="quantity" value={items} title="Qty" className="quantity" />
+                <input type="button" onClick={addItem} value="+" className="plus btn-floating btn-large waves-effect waves-light red"/>
+            </div>
+            <button onClick={() => onAdd(items)} className="waves-effect waves-light btn red">Agregar al carrito</button>  
+        </div>
+    );
+  }
+  
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,14 @@
-// import './NavBar.css';
-// import CartWidget from '../CartWidget/CartWidget';
+import ItemCount from '../ItemCount/ItemCount';
 
 function ItemListContainer (props) {
+    const onAdd = (items) => {
+        console.log(`Agregaste ${items} items`);
+    };
+    
     return (
         <div>
             {props.texto}
+            <ItemCount initial={1} stock={10} onAdd={onAdd} />
         </div>
     );
   }
