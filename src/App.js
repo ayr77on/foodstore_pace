c851e1ab03d647845a9610d6aed287ba04324f1d
Ayrton <ayrtongp77@gmail.com>
2021-06-16 18:19:26 -0300

    Agregado del router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@
 import 'materialize-css/dist/css/materialize.min.css'
 import './App.css';
 import NavBar  from './components/NavBar/NavBar';
+import { BrowserRouter ,Switch,Route,Link } from "react-router-dom";
 import ItemListContainer  from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <div className="App-header">
-      <ItemListContainer texto="Explora nuestras mejores recetas..."/>
-      <ItemDetailContainer/>
-      </div>
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+        <div className="App-header">
+          <ItemListContainer texto="Explora nuestras mejores recetas..."/>
+          <ItemDetailContainer/>
+          <Route >
+
+          </Route>
+        </div>
+        </Switch> 
+      </BrowserRouter>
     </div>
   );
 }
